935f7a9ce460f1b591f8cb4287cbabedd39be597
PerlTomas <45515200+PerlTomas@users.noreply.github.com>
2022-02-27 18:11:17 +0100

    Update MorseCodeTest.ino
    Improved comments

--- MorseCodeTest.ino
+++ MorseCodeTest.ino
@@ -1,10 +1,14 @@
+/* This is an example of how to use the MorseSWE library 
+Tomas Johansson 2022-02-27
+*/
+
 #include <MorseSWE.h>
 
 String Message="Flygande bäckasiner söka hvila på mjuka tufvor qxz 0123456789 . ? /";
 
-byte BuzzerPin=11;
-unsigned int ToneFrequency=2200; // Hz
-int CharPmin=80;
+byte BuzzerPin=11; // The Arduino pin you connect the piezo speaker to
+unsigned int ToneFrequency=2200; // Hz. Depending on your piezo speaker you might have to tweak this to get best output level.
+int CharPmin=80; // Speed in characters per minute
 
 MorseSWE Morse(BuzzerPin, ToneFrequency, CharPmin);
 
